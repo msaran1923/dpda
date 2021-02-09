48369888f2427cf51f9f35bac9307faa495980bb
msaran1923 <saran@cankaya.edu.tr>
2021-02-09 13:55:22 +0300

    Update VS2019.bat

--- DPDA,v1/VS2019.bat
+++ DPDA,v1/VS2019.bat
@@ -1,9 +1,9 @@
 @set VisualCppVersion=vc142
-@set BoostDirectory=E:\Boost\
+@set BoostDirectory=D:\Boost\
 @set BoostVersion=1_74
-@set OpenCVDirectory=E:\OpenCV\
+@set OpenCVDirectory=D:\OpenCV\
 @set OpenCVVersion=440
-@set PATH=%PATH%;E:\OpenCV\build\x64\vc15\bin;C:\Program Files\ImageMagick-7.0.10-Q16-HDRI
+@set PATH=%PATH%;D:\OpenCV\build\x64\vc15\bin;C:\Program Files\ImageMagick-7.0.10-Q16-HDRI
 
 @C:
 @cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE
