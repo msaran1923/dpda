95031c0ccc12436eb26b571d2355dee5b66ba3a2
msaran1923 <saran@cankaya.edu.tr>
2021-04-16 11:49:55 +0300

    Add files via upload

---
+++ VS2019.bat
@@ -0,0 +1,10 @@
+@set VisualCppVersion=vc142
+@set BoostDirectory=C:\Boost\
+@set BoostVersion=1_74
+@set OpenCVDirectory=C:\OpenCV\
+@set OpenCVVersion=440
+@set PATH=%PATH%;C:\OpenCV\build\x64\vc15\bin;C:\Program Files\ImageMagick-7.0.10-Q16-HDRI
+
+@C:
+@cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE
+@start devenv.exe
